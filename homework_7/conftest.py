33ac0f4aae5dcfb130323502eae69a67612c5208
Alexander Krayushkin <marcusowley@gmail.com>
2024-09-18 18:12:19 +0300

    Внес исправления по комментариям Сергея.

--- homework_7/conftest.py
+++ homework_7/conftest.py
@@ -4,7 +4,34 @@ import pytest
 
 
 @pytest.fixture
-def add_in_zip_fixture():
+def create_tmp_dir():
+    tmp_dir = 'tmp'
+    if not os.path.exists(tmp_dir):
+        os.makedirs(tmp_dir)
+    return tmp_dir
+
+
+@pytest.fixture
+def create_test_files(create_tmp_dir):
+    tmp_dir = create_tmp_dir
+
+    files_content = {
+        'pdf.pdf': b'This is PDF file',
+        'csv.csv': b'This is CSV file',
+        'xlsx.xlsx': b'This is XLSX file'
+    }
+    file_paths = []
+    for filename, content in files_content.items():
+        file_path = os.path.join(tmp_dir, filename)
+        with open(file_path, 'wb') as f:
+            f.write(content)
+        file_paths.append(file_path)
+
+    return file_paths
+
+
+@pytest.fixture
+def add_in_zip():
     def add_in_zip(zip_name, files):
         with zipfile.ZipFile(zip_name, 'a') as zip_write:
             for file in files:
@@ -14,7 +41,7 @@ def add_in_zip_fixture():
 
 
 @pytest.fixture
-def read_file_content_in_zip_fixture():
+def read_file_content_in_zip():
     def read_file_content_in_zip(zip_name, file):
         with zipfile.ZipFile(zip_name, 'r') as zip_read:
             with zip_read.open(file) as file:
@@ -25,7 +52,7 @@ def read_file_content_in_zip_fixture():
 
 
 @pytest.fixture
-def delete_zip_fixture():
+def delete_zip():
     def delete_zip(zip_name):
         if os.path.exists(zip_name):
             os.remove(zip_name)
--- homework_7/test_hw_7.py
+++ homework_7/test_hw_7.py
@@ -1,38 +1,13 @@
 import zipfile
 import os
 
-def create_tmp_dir():
-    tmp_dir = 'tmp'
-    if not os.path.exists(tmp_dir):
-        os.makedirs(tmp_dir)
-    return tmp_dir
+def test_zip_file(add_in_zip, read_file_content_in_zip, delete_zip, create_test_files):
 
-def create_test_files():
-    tmp_dir = create_tmp_dir()
+    name_files = create_test_files
 
-    files_content = {
-        'pdf.pdf': b'This is PDF file',
-        'csv.csv': b'This is CSV file',
-        'xlsx.xlsx': b'This is XLSX file'
-    }
-    file_paths = []
-    for filename, content in files_content.items():
-        file_path = os.path.join(tmp_dir, filename)
-        with open(file_path, 'wb') as f:
-            f.write(content)
-        file_paths.append(file_path)
-
-    return file_paths
-
-def test_zip_file(add_in_zip_fixture, read_file_content_in_zip_fixture, delete_zip_fixture):
-    # Создание тестовых файлов
-    name_files = create_test_files()
-
-    # Название для zip файла в папке tmp
     zip_file_name = os.path.join('tmp', 'zip_with_files.zip')
 
-    # Вызов добавления файлов в zip
-    add_in_zip_fixture(zip_file_name, name_files)
+    add_in_zip(zip_file_name, name_files)
 
     # Проверка корректности добавления файлов
     with zipfile.ZipFile(zip_file_name, 'r') as zipf:
@@ -42,13 +17,12 @@ def test_zip_file(add_in_zip_fixture, read_file_content_in_zip_fixture, delete_z
         expected_files = [os.path.basename(file) for file in name_files]
         assert sorted(zip_content_files) == sorted(expected_files)
 
-    # Проверка содержимого файлов относительно самих файлов
     content_files = []
     for item in name_files:
         # Находим полный путь файла в архиве
         full_path_in_zip = [file for file in zip_content if os.path.basename(file) == os.path.basename(item)]
         if full_path_in_zip:
-            content = read_file_content_in_zip_fixture(zip_file_name, full_path_in_zip[0])
+            content = read_file_content_in_zip(zip_file_name, full_path_in_zip[0])
             content_files.append(content)
             if os.path.basename(item) == 'csv.csv':
                 assert content == 'This is CSV file'
@@ -57,11 +31,9 @@ def test_zip_file(add_in_zip_fixture, read_file_content_in_zip_fixture, delete_z
             elif os.path.basename(item) == 'xlsx.xlsx':
                 assert content == 'This is XLSX file'
 
-    # Общая проверка содержимого всех файлов
     assert content_files == ['This is PDF file', 'This is CSV file', 'This is XLSX file']
 
-    # Удаление zip файла и тестовых файлов после проверки
-    delete_zip_fixture(zip_file_name)
+    delete_zip(zip_file_name)
     for file in name_files:
         if os.path.exists(file):
             os.remove(file)
